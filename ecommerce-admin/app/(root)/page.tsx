fb7fb11e97efffdc767f8b2e6fd7dcf67162e7e9
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-10-20 10:45:13 +0300

    changed the setup page

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,7 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-    return (
-        <div className="p-4 ">
-            <Button>Hello</Button>
-        </div>
-    );
-}
+const SetupPage = () => {
+    return <div className="p-4 ">this is a protected route</div>;
+};
+
+export default SetupPage;
