849894d3529e8f568946995e916e095adde4c6e2
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-11-06 12:14:31 +0200

    updated lines a while ago

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
-    return <div className="p-4 ">this is a protected route</div>;
+    return (
+        <div className="p-4 ">
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
 };
 
 export default SetupPage;
